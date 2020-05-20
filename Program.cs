b986427ba92415ae5fb16dd0536a3d3eb7c27b7c
Celeo <mattboulanger@fastmail.com>
2020-05-19 19:57:36 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1) {
+            Console.WriteLine("Must be called with the path to the .plr file");
+            return;
+        }
+        decryptFile(args[0], "terraria.plr.decrypted");
+    }
+
+    static void decryptFile(string inputFile, string outputFile)
+    {
+        string s = "h3y_gUyZ";
+        UnicodeEncoding unicodeEncoding = new UnicodeEncoding();
+        byte[] bytes = unicodeEncoding.GetBytes(s);
+        FileStream fileStream = new FileStream(inputFile, FileMode.Open);
+        RijndaelManaged rijndaelManaged = new RijndaelManaged();
+        CryptoStream cryptoStream = new CryptoStream(fileStream, rijndaelManaged.CreateDecryptor(bytes, bytes), CryptoStreamMode.Read);
+        FileStream fileStream2 = new FileStream(outputFile, FileMode.Create);
+        try
+        {
+            int num;
+            while ((num = cryptoStream.ReadByte()) != -1)
+            {
+                fileStream2.WriteByte((byte) num);
+            }
+            fileStream2.Close();
+            cryptoStream.Close();
+            fileStream.Close();
+        }
+        catch
+        {
+            fileStream2.Close();
+            fileStream.Close();
+            File.Delete(outputFile);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# terraria_plr_decrypt
+
+Simple C# dotnet project to decrypt Terraria .plr files.
+
+**Disclaimer**: I am **not** a C# dev anymore.
+
+## Usage
+
+```sh
+terraria_plr_decrypt path/to/Name.plr
+```
+
+## Building
+
+```sh
+git clone https://github.com/Celeo/terraria_plr_decrypt
+cd terraria_plr_decrypt
+dotnet build
+```
